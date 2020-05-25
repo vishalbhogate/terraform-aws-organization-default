21db349aa2a75f507ec03a9581227c2e22493531
vishal <vishal.bhogate07@gmail.com>
2020-05-25 16:47:56 +1000

    Organization stack

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraform-aws-organization-default
 Terraform module to create a AWS Organization
+
+Backend.tf and Provider.tf should be the part of module
---
+++ policy.tf
@@ -0,0 +1,30 @@
+resource aws_organizations_policy "config" {
+  count = var.already_exists ? 0 : 1
+  name  = "config"
+
+  content = <<CONTENT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Deny",
+      "Action": [
+        "config:DeleteConfigRule",
+        "config:DeleteConfigurationRecorder",
+        "config:DeleteDeliveryChannel",
+        "config:StopConfigurationRecorder"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+CONTENT
+
+  depends_on = [aws_organizations_organization.org[0]]
+
+}
+
+resource aws_organizations_policy_attachment "config_attachment" {
+  policy_id = aws_organizations_policy.config[0].id
+  target_id = aws_organizations_organization.org[0].roots[0]["id"]
+}
